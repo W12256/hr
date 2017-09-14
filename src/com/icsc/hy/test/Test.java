667531ed2bbff49de850fc13e4dbe73bbb55cfa3
W12256 <31919133+W12256@users.noreply.github.com>
2017-09-14 16:06:28 +0800

    Update Test.java

--- src/com/icsc/hy/test/Test.java
+++ src/com/icsc/hy/test/Test.java
@@ -3,5 +3,6 @@ package com.icsc.hy.test;
 public class Test {
          public static void main(String[] args) {
 			System.out.println("Hello Java!");
+                        System.out.println("I CAN DO THIS!");
 		}
 }
