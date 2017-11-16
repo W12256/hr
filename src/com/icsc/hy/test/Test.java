c0fd9403681206f9abeb3e342578bbe31aa34b9b
W12256 <W12256@icsc.com.tw>
2017-11-16 16:49:02 +0800

    1

--- src/com/icsc/hy/test/Test.java
+++ src/com/icsc/hy/test/Test.java
@@ -2,7 +2,6 @@ package com.icsc.hy.test;
 
 public class Test {
          public static void main(String[] args) {
-			System.out.println("Hello Java!");
-                        System.out.println("I CAN DO THIS!");
+            System.out.println("I CAN DO THIS!");
 		}
 }
